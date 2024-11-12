e4681fc91b319743c387318513fe05a03c910e51
manognak6 <110725358+manognak6@users.noreply.github.com>
2024-11-12 15:41:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.studentdetailsapp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("**********Student Management System**********");
+		
+
+	}
+
+}
---
+++ Student.java
@@ -0,0 +1,12 @@
+package com.studentdetailsapp;
+
+import java.util.List;
+
+public class Student {
+	String name;
+	int age;
+	String studentId;
+	List<String> courses;
+	
+
+}
